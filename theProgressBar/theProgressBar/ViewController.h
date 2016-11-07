bf57f4a6f54a85c8c37364e2bfa11840c44eb9c5
zengsp <845353553@qq.com>
2016-11-07 15:33:16 +0800

    progressBar1.0

---
+++ theProgressBar/theProgressBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  theProgressBar
+//
+//  Created by 曾绍鹏 on 2016/11/7.
+//  Copyright © 2016年 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ theProgressBar/theProgressBar/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  theProgressBar
+//
+//  Created by 曾绍鹏 on 2016/11/7.
+//  Copyright © 2016年 Paul. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
